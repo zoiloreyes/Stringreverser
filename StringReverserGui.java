b9f68b9b84e89047b0a7a87c5ff063e9f1654b26
zoiloreyes <zoiloismaelreyes1@gmail.com>
2016-08-09 11:46:16 -0400

    Updated StringReverser

--- StringReverserGui.java
+++ StringReverserGui.java
@@ -5,7 +5,7 @@ public class StringReverserGui implements ActionListener, KeyListener{
 	
 	JFrame frame = new JFrame("gnirtS Reverser");
 	JButton button = new JButton("Reverse String");
-	JTextField text = new JTextField("Enter your String");
+	JTextField text = new JTextField("Enter your String                               ");
 	JLabel message = new JLabel("Welcome to the String reverser ");
 	JLabel result = new JLabel(" ");
 	JPanel panel = new JPanel();
@@ -17,17 +17,23 @@ public class StringReverserGui implements ActionListener, KeyListener{
 		srg.start();
 	}
 	public void start(){
-		BoxLayout bl = new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS);
+		BorderLayout bl = new BorderLayout();
+		GridBagLayout gl = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		c.weightx = 1.0;
+		c.fill=GridBagConstraints.HORIZONTAL;
+		panel.setLayout(gl);
 		frame.setLayout(bl);
 		button.addActionListener(this);
 		button.addKeyListener(this);
 		text.addKeyListener(this);
 
+		panel.add(text);
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(message);
-		frame.add(text);
-		frame.add(result);
-		frame.add(button);
+		frame.add(panel,BorderLayout.NORTH);
+		frame.add(result,BorderLayout.CENTER);
+		frame.add(button,BorderLayout.SOUTH);
 		frame.pack();
 		frame.setSize(400,400);
 		frame.setVisible(true);
